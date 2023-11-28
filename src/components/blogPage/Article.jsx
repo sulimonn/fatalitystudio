827e78222df8a91605c9f306b89f92a47ec95bd7
sulimonn <gsuli836@gmail.com>
2023-11-28 20:10:12 +0600

    spinner

--- src/components/blogPage/Article.jsx
+++ src/components/blogPage/Article.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 import BtnSimple from '../utils/btn/BtnSimple';
 
 function Article({ data }) {
+  const [loading, setLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
   return (
     <div className="article">
       <div className="main-article__img">
-        <img src={require(`../../images/blog/${data.src}`)} alt="apicture" />
+        {loading && (
+          <div className="img">
+            <span className="spinner-border m-5" role="status"></span>
+          </div>
+        )}
+        <img
+          className="img"
+          onLoad={handleImageLoad}
+          src={require(`../../images/blog/${data.src}`)}
+          alt="apicture"
+          style={{ display: loading ? 'none' : 'block' }}
+        />
       </div>
       <div className="article__info">
         <p className="date dsc1">{data.date}</p>
