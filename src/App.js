82f9465c831213a1295526ebe0d5fc7cd4d5018b
sulimonn <gsuli836@gmail.com>
2023-11-29 22:00:34 +0600

    partciles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import './components/utils/typography/typography.css';
 import IntroPage from './components/intropage/Intropage';
-import Particles from 'react-particles';
-import { loadSlim } from 'tsparticles-slim';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import FeedbackPage from './components/feedbackPage/FeedbackPage';
@@ -13,11 +11,8 @@ import PorrtfoliosPage from './components/portfolioPage/PortfoliosPage';
 import Article from './components/articlePage/Article';
 import Project from './components/project/Project';
 import ServicePage from './components/servicePage/ServicePage';
-
+import ParticlesComponent from './components/PartclesComponent';
 function App() {
-  const particlesInit = useCallback(async (engine) => {
-    await loadSlim(engine);
-  }, []);
   const shouldDisplayHeader = !window.location.pathname.startsWith('/admin');
 
   return (
@@ -41,70 +36,7 @@ function App() {
         </div>
         {shouldDisplayHeader && <Contact />}
         {shouldDisplayHeader && <Footer />}
-        <Particles
-          id="particles"
-          init={particlesInit}
-          options={{
-            fullScreen: false,
-            background: {
-              color: {
-                value: 'transparent',
-              },
-            },
-            fpsLimit: 20,
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: false,
-                  mode: 'push',
-                },
-                onHover: {
-                  enable: false,
-                  mode: 'repulse',
-                },
-              },
-            },
-            particles: {
-              color: {
-                value: 'rgba(255, 255, 255, 0.3)',
-              },
-              links: {
-                color: '#ffffff',
-                distance: 140,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-              },
-              move: {
-                direction: 'none',
-                enable: true,
-                outModes: {
-                  default: 'bounce',
-                },
-                random: false,
-                speed: 1,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 900,
-                },
-                value: 60,
-              },
-              opacity: {
-                value: 0.3,
-              },
-              shape: {
-                type: 'circle',
-              },
-              size: {
-                value: { min: 1, max: 5 },
-              },
-            },
-            detectRetina: true,
-          }}
-        />
+        <ParticlesComponent />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/PartclesComponent.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import 'particles.js';
+
+function ParticlesComponent() {
+  useEffect(() => {
+    const particlesJS = window.particlesJS;
+
+    particlesJS('particles-js', {
+      particles: {
+        number: {
+          value: 80,
+          density: {
+            enable: true,
+            value_area: 920,
+          },
+        },
+        size: {
+          value: 6,
+          random: true,
+        },
+        opacity: {
+          value: 0.2,
+        },
+        move: {
+          speed: 3,
+        },
+        line_linked: {
+          opacity: 0.5,
+          width: 1,
+          distance: 140,
+        },
+      },
+    });
+  }, []);
+  return <div id="particles-js"></div>;
+}
+
+export default ParticlesComponent;
