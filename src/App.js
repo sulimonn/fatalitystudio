4dc0d6699ae52f2cc3efc72cab970e009354e933
sulimonn <gsuli836@gmail.com>
2024-02-03 23:58:30 +0600

    deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/header/Header';
 import './components/utils/typography/typography.css';
 import IntroPage from './components/intropage/Intropage';
 import Contact from './components/contact/Contact';
@@ -12,31 +11,57 @@ import Article from './components/articlePage/Article';
 import Project from './components/project/Project';
 import ServicePage from './components/servicePage/ServicePage';
 import ParticlesComponent from './components/PartclesComponent';
-function App() {
-  const shouldDisplayHeader = !window.location.pathname.startsWith('/admin');
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+function App() {
+  const defaultTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      background: '#242424',
+      primary: {
+        main: '#A9A9A9',
+        light: '#FFFFFF',
+        dark: '#242424',
+        contrastText: '#141416',
+      },
+      warning: {
+        main: '#FFD951',
+      },
+      info: {
+        main: '#08FFFC10',
+        light: '#08FFFC',
+      },
+      secondary: {
+        main: '#ff4081',
+      },
+    },
+    typography: {
+      fontFamily: 'var(--ff-exo-2)',
+    },
+  });
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <div className="App">
-        {shouldDisplayHeader && <Header />}
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<IntroPage />}></Route>
-            <Route path="/feedback" element={<FeedbackPage />}></Route>
-            <Route path="/blog/*">
-              <Route index element={<Blog />} />
-              <Route path=":id" element={<Article />}></Route>
-            </Route>
-            <Route path="/portfolio/*">
-              <Route index element={<PorrtfoliosPage />} />
-              <Route path=":id" element={<Project />} />
-            </Route>
-            <Route path="/services/:id" element={<ServicePage />} />
-          </Routes>
-        </div>
-        {shouldDisplayHeader && <Contact />}
-        {shouldDisplayHeader && <Footer />}
-        <ParticlesComponent />
+        <ThemeProvider theme={defaultTheme}>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<IntroPage />}></Route>
+              <Route path="/feedback" element={<FeedbackPage />}></Route>
+              <Route path="/blog/*">
+                <Route index element={<Blog />} />
+                <Route path=":id" element={<Article />}></Route>
+              </Route>
+              <Route path="/portfolio/*">
+                <Route index element={<PorrtfoliosPage />} />
+                <Route path=":id" element={<Project />} />
+              </Route>
+              <Route path="/services/:id" element={<ServicePage />} />
+            </Routes>
+          </div>
+          <Contact />
+          <Footer />
+          <ParticlesComponent />
+        </ThemeProvider>
       </div>
     </BrowserRouter>
   );
--- src/components/intropage/ourteam/TeamCard.jsx
+++ src/components/intropage/ourteam/TeamCard.jsx
@@ -4,10 +4,12 @@ function TeamCard({ data }) {
   return (
     <div className="ourteam-item row">
       <div className="img col-lg-4">
-        <img src={require('../../../images/team/' + data.src)} alt="" />
+        <img src={require('../../../images/team/' + data.avatar)} alt="" />
       </div>
       <div className="info col-lg-8">
-        <h6 className="dsc1">{data.name}</h6>
+        <h6 className="dsc1">
+          {data.firstName} {data.lastName}
+        </h6>
         <p className="paragraph">{data.position}</p>
       </div>
     </div>
